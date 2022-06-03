2b5d024e7f3247f37c8c25be702d3293eae89a00
konstantin <kosvasl09@gmail.com>
2022-06-03 16:10:35 +0900

    добавлен комментарий

--- data/src/main/java/com/example/data/network/RetrofitService.kt
+++ data/src/main/java/com/example/data/network/RetrofitService.kt
@@ -9,6 +9,11 @@ interface RetrofitService {
 
     @GET("/v2/top-headlines?country=us")
     suspend fun getBreakingNews(
+        /**
+         * Для получения данных из REST API необходимо прописать в local.properties
+         * слудеющюю строку: NEWS_API_KEY="e8f656fe729842b792d2cbb5d2180b93",
+         * либо за хардкодить здесь
+         */
         @Query("apikey") apiKey: String = BuildConfig.NEWS_API_KEY,
     ): BreakingNewsDTO
 }
